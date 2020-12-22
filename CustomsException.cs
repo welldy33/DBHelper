0312365b4983576a2ae431f5babb798b7da6dd2c
welldy33 <diezer33@gmail.com>
2020-12-22 10:47:52 +0700

    add customs exceptions

---
+++ CustomsException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Helper
+{
+    [Serializable]
+    public class CustomsException:Exception
+    {
+        //    private Dictionary<string, object> _args;
+        //    public CustomsException(string msg, params object[] args) : base(msg) {
+        //        this._args = new Dictionary<string, object>();
+        //        for (int index = 0; index < args.Length - 1; index += 2)
+        //            this._args[args.Length > index + 1 ? args[index].ToString() : "ARG" + (object)(index / 2 + 1)] = args.Length > index + 1 ? args[index + 1] : args[index];
+        //    }
+        //    public Dictionary<string, object> Args => this._args;
+        public CustomsException()
+        {
+        }
+
+        public CustomsException(string message)
+          : base(message)
+        {
+        }
+        public CustomsException(Exception exception) :
+                     this(exception.Message)
+        {
+        }
+        public CustomsException(string message, Exception innerException)
+        : base(message, innerException)
+        {
+        }
+    }
+}
